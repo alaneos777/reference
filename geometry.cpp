f772f7a0df19eaf79bfaad8ce6b239e02bc1d54c
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2018-09-07 22:27:26 -0500

    Update

--- geometry.cpp
+++ geometry.cpp
@@ -12,6 +12,8 @@ struct point{
 
 	point(double x, double y): x(x), y(y){}
 
+	void read(){cin >> x >> y;}
+
 	point operator+(const point & p) const{return point(x + p.x, y + p.y);}
 	
 	point operator-(const point & p) const{return point(x - p.x, y - p.y);}
@@ -33,6 +35,9 @@ struct point{
 	}
 	point rotate(const double angle, const point & p){
 		return p + ((*this) - p).rotate(angle);
+	}
+	point perpendicular() const{
+		return point(-y, x);
 	}
 
 	double dot(const point & p) const{
@@ -63,18 +68,12 @@ struct point{
 		return !(*this == p);
 	}
 	bool operator<(const point & p) const{
-		if(abs(x - p.x) < eps){
-			return y < p.y;
-		}else{
-			return x < p.x;
-		}
+		if(abs(x - p.x) < eps) return y < p.y;
+		return x < p.x;
 	}
 	bool operator>(const point & p) const{
-		if(abs(x - p.x) < eps){
-			return y > p.y;
-		}else{
-			return x > p.x;
-		}
+		if(abs(x - p.x) < eps) return y > p.y;
+		return x > p.x;
 	}
 };
 
@@ -86,7 +85,7 @@ istream &operator>>(istream &is, point & P){
 }
 
 ostream &operator<<(ostream &os, const point & p) { 
-    return os << fixed << setprecision(8) << p.x << " " << p.y;
+    return os << "(" << p.x << ", " << p.y << ")";
 }
 
 int sgn(double x){
@@ -99,23 +98,24 @@ int sgn(double x){
 	}
 }
 
-bool pointInLine(point & a, point & b, point & p){
-	//line ab, point p
-	return abs((p - a).cross(b - a)) < eps;
+
+bool pointInLine(const point & a, const point & v, const point & p){
+	//line a+tv, point p
+	return abs((p - a).cross(v)) < eps;
 }
 
-bool pointInSegment(point a, point b, point & p){
+bool pointInSegment(point a, point b, const point & p){
 	//segment ab, point p
 	if(a > b) swap(a, b);
-	return pointInLine(a, b, p) && !(p < a || p > b);
+	return pointInLine(a, b - a, p) && !(p < a || p > b);
 }
 
-int intersectLinesInfo(point & a, point & b, point & c, point & d){
-	//line ab, line cd
-	point v1 = b - a, v2 = d - c;
+int intersectLinesInfo(const point & a1, const point & v1, const point & a2, const point & v2){
+	//line a1+tv1
+	//line a2+tv2
 	double det = v1.cross(v2);
 	if(abs(det) < eps){
-		if(abs((c - a).cross(v1)) < eps){
+		if(abs((a2 - a1).cross(v1)) < eps){
 			return -1; //infinity points
 		}else{
 			return 0; //no points
@@ -125,29 +125,29 @@ int intersectLinesInfo(point & a, point & b, point & c, point & d){
 	}
 }
 
-point intersectLines(point & a, point & b, point & c, point & d){
+point intersectLines(const point & a1, const point & v1, const point & a2, const point & v2){
+	//lines a1+tv1, a2+tv2
 	//assuming that they intersect
-	point v1 = b - a, v2 = d - c;
 	double det = v1.cross(v2);
-	return a + v1 * ((c - a).cross(v2) / det);
+	return a1 + v1 * ((a2 - a1).cross(v2) / det);
 }
 
-int intersectLineSegmentInfo(point & a, point & b, point & c, point & d){
-	//line ab, segment cd
-	point v1 = b - a, v2 = d - c;
-	double det = v1.cross(v2);
+int intersectLineSegmentInfo(const point & a, const point & v, const point & c, const point & d){
+	//line a+tv, segment cd
+	point v2 = d - c;
+	double det = v.cross(v2);
 	if(abs(det) < eps){
-		if(abs((c - a).cross(v1)) < eps){
+		if(abs((c - a).cross(v)) < eps){
 			return -1; //infinity points
 		}else{
 			return 0; //no point
 		}
 	}else{
-		return sgn(v1.cross(c - a)) != sgn(v1.cross(d - a)); //1: single point, 0: no point
+		return sgn(v.cross(c - a)) != sgn(v.cross(d - a)); //1: single point, 0: no point
 	}
 }
 
-int intersectSegmentsInfo(point & a, point & b, point & c, point & d){
+int intersectSegmentsInfo(const point & a, const point & b, const point & c, const point & d){
 	//segment ab, segment cd
 	point v1 = b - a, v2 = d - c;
 	int t = sgn(v1.cross(c - a)), u = sgn(v1.cross(d - a));
@@ -166,43 +166,43 @@ int intersectSegmentsInfo(point & a, point & b, point & c, point & d){
 	}
 }
 
-double distancePointLine(point & a, point & v, point & p){
+double distancePointLine(const point & a, const point & v, const point & p){
 	//line: a + tv, point p
 	return abs(v.cross(p - a)) / v.length();
 }
 
-double perimeter(vector<point> & points){
-	int n = points.size();
+double perimeter(vector<point> & P){
+	int n = P.size();
 	double ans = 0;
 	for(int i = 0; i < n; i++){
-		ans += (points[i] - points[(i + 1) % n]).length();
+		ans += (P[i] - P[(i + 1) % n]).length();
 	}
 	return ans;
 }
 
-double area(vector<point> & points){
-	int n = points.size();
+double area(vector<point> & P){
+	int n = P.size();
 	double ans = 0;
 	for(int i = 0; i < n; i++){
-		ans += points[i].cross(points[(i + 1) % n]);
+		ans += P[i].cross(P[(i + 1) % n]);
 	}
 	return abs(ans / 2);
 }
 
-vector<point> convexHull(vector<point> points){
-	sort(points.begin(), points.end());
+vector<point> convexHull(vector<point> P){
+	sort(P.begin(), P.end());
 	vector<point> L, U;
-	for(int i = 0; i < points.size(); i++){
-		while(L.size() >= 2 && (L[L.size() - 2] - points[i]).cross(L[L.size() - 1] - points[i]) <= 0){
+	for(int i = 0; i < P.size(); i++){
+		while(L.size() >= 2 && (L[L.size() - 2] - P[i]).cross(L[L.size() - 1] - P[i]) <= 0){
 			L.pop_back();
 		}
-		L.push_back(points[i]);
+		L.push_back(P[i]);
 	}
-	for(int i = points.size() - 1; i >= 0; i--){
-		while(U.size() >= 2 && (U[U.size() - 2] - points[i]).cross(U[U.size() - 1] - points[i]) <= 0){
+	for(int i = P.size() - 1; i >= 0; i--){
+		while(U.size() >= 2 && (U[U.size() - 2] - P[i]).cross(U[U.size() - 1] - P[i]) <= 0){
 			U.pop_back();
 		}
-		U.push_back(points[i]);
+		U.push_back(P[i]);
 	}
 	L.pop_back();
 	U.pop_back();
@@ -210,25 +210,25 @@ vector<point> convexHull(vector<point> points){
 	return L;
 }
 
-bool pointInPerimeter(vector<point> & points, point & p){
-	int n = points.size();
+bool pointInPerimeter(vector<point> & P, const point & p){
+	int n = P.size();
 	for(int i = 0; i < n; i++){
-		if(pointInSegment(points[i], points[(i + 1) % n], p)){
+		if(pointInSegment(P[i], P[(i + 1) % n], p)){
 			return true;
 		}
 	}
 	return false;
 }
 
-int pointInPolygon(vector<point> & points, point & p){
-	if(pointInPerimeter(points, p)){
+int pointInPolygon(vector<point> & P, const point & p){
+	if(pointInPerimeter(P, p)){
 		return -1; //point in the perimeter
 	}
-	point bottomLeft = (*min_element(points.begin(), points.end())) - point(M_E, M_PI);
-	int n = points.size();
+	point bottomLeft = (*min_element(P.begin(), P.end())) - point(M_E, M_PI);
+	int n = P.size();
 	int rays = 0;
 	for(int i = 0; i < n; i++){
-		rays += (intersectSegmentsInfo(p, bottomLeft, points[i], points[(i + 1) % n]) == 1 ? 1 : 0);
+		rays += (intersectSegmentsInfo(p, bottomLeft, P[i], P[(i + 1) % n]) == 1 ? 1 : 0);
 	}
 	return rays & 1; //0: point outside, 1: point inside
 }
@@ -236,41 +236,41 @@ int pointInPolygon(vector<point> & points, point & p){
 bool comp1(const point & a, const point & b){
 	return a.y < b.y;
 }
-pair<point, point> closestPairOfPoints(vector<point> points){
-	sort(points.begin(), points.end(), comp1);
+pair<point, point> closestPairOfPoints(vector<point> P){
+	sort(P.begin(), P.end(), comp1);
 	set<point> S;
 	double ans = 1e9;
 	point p, q;
 	int pos = 0;
-	for(int i = 0; i < points.size(); ++i){
-		while(pos < i && abs(points[i].y - points[pos].y) >= ans){
-			S.erase(points[pos++]);
+	for(int i = 0; i < P.size(); ++i){
+		while(pos < i && abs(P[i].y - P[pos].y) >= ans){
+			S.erase(P[pos++]);
 		}
-		auto lower = S.lower_bound({points[i].x - ans - eps, -1e9});
-		auto upper = S.upper_bound({points[i].x + ans + eps, -1e9});
+		auto lower = S.lower_bound({P[i].x - ans - eps, -1e9});
+		auto upper = S.upper_bound({P[i].x + ans + eps, -1e9});
 		for(auto it = lower; it != upper; ++it){
-			double d = (points[i] - *it).length();
+			double d = (P[i] - *it).length();
 			if(d < ans){
 				ans = d;
-				p = points[i];
+				p = P[i];
 				q = *it;
 			}
 		}
-		S.insert(points[i]);
+		S.insert(P[i]);
 	}
 	return {p, q};
 }
 
-point centroid(vector<point> & points){
-	point P;
-	double div = 0;
-	int n = points.size();
+point centroid(vector<point> & P){
+	point num;
+	double den = 0;
+	int n = P.size();
 	for(int i = 0; i < n; ++i){
-		double cross = points[i].cross(points[(i + 1) % n]);
-		P += (points[i] + points[(i + 1) % n]) * cross;
-		div += cross;
+		double cross = P[i].cross(P[(i + 1) % n]);
+		num += (P[i] + P[(i + 1) % n]) * cross;
+		den += cross;
 	}
-	return P / (3.0 * div);
+	return num / (3.0 * den);
 }
 
 struct vantage_point_tree{
@@ -338,12 +338,208 @@ struct vantage_point_tree{
 	}
 };
 
-int main(){
-	int n;
-	cin >> n;
-	vector<point> poly(n);
-	for(int i = 0; i < n; ++i){
-		cin >> poly[i];
+vector<pair<int, int>> antipodalPairs(vector<point> & P){
+	vector<pair<int, int>> ans;
+	int n = P.size(), k = 1;
+	auto f = [&](int u, int v, int w){return abs((P[v%n]-P[u%n]).cross(P[w%n]-P[u%n]));};
+	while(f(n-1, 0, k+1) > f(n-1, 0, k)) ++k;
+	for(int i = 0, j = k; i <= k && j < n; ++i){
+		ans.emplace_back(i, j);
+		while(j < n-1 && f(i, i+1, j+1) > f(i, i+1, j))
+			ans.emplace_back(i, ++j);
 	}
-	cout << centroid(poly) << "\n";
+	return ans;
+}
+
+pair<double, double> diameterAndWidth(vector<point> & P){
+	int n = P.size(), k = 0;
+	auto dot = [&](int a, int b){return (P[(a+1)%n]-P[a]).dot(P[(b+1)%n]-P[b]);};
+	auto cross = [&](int a, int b){return (P[(a+1)%n]-P[a]).cross(P[(b+1)%n]-P[b]);};
+	double diameter = numeric_limits<double>::min();
+	double width = numeric_limits<double>::max();
+	while(dot(0, k) > eps) k = (k+1) % n;
+	for(int i = 0; i < n; ++i){
+		while(cross(i, k) > eps) k = (k+1) % n;
+		//pair: (i, k)
+		diameter = max(diameter, (P[k] - P[i]).length());
+		width = min(width, distancePointLine(P[i], P[(i+1)%n] - P[i], P[k]));
+	}
+	return make_pair(diameter, width);
+}
+
+pair<double, double> smallestEnclosingRectangle(vector<point> & P){
+	int n = P.size();
+	auto dot = [&](int a, int b){return (P[(a+1)%n]-P[a]).dot(P[(b+1)%n]-P[b]);};
+	auto cross = [&](int a, int b){return (P[(a+1)%n]-P[a]).cross(P[(b+1)%n]-P[b]);};
+	double perimeter = numeric_limits<double>::max(), area = perimeter;
+	for(int i = 0, j = 0, k = 0, m = 0; i < n; ++i){
+		while(dot(i, j) > eps) j = (j+1) % n;
+		if(!i) k = j;
+		while(cross(i, k) > eps) k = (k+1) % n;
+		if(!i) m = k;
+		while(dot(i, m) < -eps) m = (m+1) % n;
+		//pairs: (i, k) , (j, m)
+		point v = P[(i+1)%n] - P[i];
+		double h = distancePointLine(P[i], v, P[k]);
+		double w = distancePointLine(P[j], v.perpendicular(), P[m]);
+		perimeter = min(perimeter, 2 * (h + w));
+		area = min(area, h * w);
+	}
+	return make_pair(area, perimeter);
+}
+
+double distancePointCircle(const point & p, const point & c, double r){
+	//point p, center c, radius r
+	return max(0.0, (p - c).length() - r);
+}
+
+point projectionPointCircle(const point & p, const point & c, double r){
+	//point p (outside the circle), center c, radius r
+	return c + (p - c) / (p - c).length() * r;
+}
+
+pair<point, point> pointsOfTangency(const point & p, const point & c, double r){
+	//point p (outside the circle), center c, radius r
+	point v = (p - c).normalize() * r;
+	double theta = acos(r / (p - c).length());
+	return {c + v.rotate(-theta), c + v.rotate(theta)};
+}
+
+vector<point> intersectLineCircle(const point & a, const point & v, const point & c, double r){
+	//line a+tv, center c, radius r
+	double A = v.dot(v);
+	double B = (a - c).dot(v);
+	double C = (a - c).dot(a - c) - r * r;
+	double D = B*B - A*C;
+	if(abs(D) < eps) return {a + v * (-B/A)}; //line tangent to circle
+	else if(D < 0) return {}; //no intersection
+	else{ //two points of intersection (chord)
+		D = sqrt(D);
+		double t1 = (-B + D) / A;
+		double t2 = (-B - D) / A;
+		return {a + v * t1, a + v * t2};
+	}
+}
+
+pair<point, double> getCircle(const point & m, const point & n, const point & p){
+	//find circle that passes through points p, q, r
+	point c = intersectLines((n + m) / 2, (n - m).perpendicular(), (p + n) / 2, (p - n).perpendicular());
+	double r = (c - m).length();
+	return {c, r};
+}
+
+vector<point> intersectionCircles(const point & c1, double r1, const point & c2, double r2){
+	//circle 1 with center c1 and radius r1
+	//circle 2 with center c2 and radius r2
+	double A = 2*r1*(c2.y - c1.y);
+	double B = 2*r1*(c2.x - c1.x);
+	double C = (c1 - c2).dot(c1 - c2) + r1*r1 - r2*r2;
+	double D = A*A + B*B - C*C;
+	if(abs(D) < eps) return {c1 + point(B, A) * r1 / C};
+	else if(D < 0) return {};
+	else{
+		D = sqrt(D);
+		double cos1 = (B*C + A*D) / (A*A + B*B);
+		double sin1 = (A*C - B*D) / (A*A + B*B);
+		double cos2 = (B*C - A*D) / (A*A + B*B);
+		double sin2 = (A*C + B*D) / (A*A + B*B);
+		return {c1 + point(cos1, sin1) * r1, c1 + point(cos2, sin2) * r1};
+	}
+}
+
+int circleInsideCircle(const point & c1, double r1, const point & c2, double r2){
+	//test if circle 2 is inside circle 1
+	//returns "-1" if 2 touches internally 1, "1" if 2 is inside 1, "0" if they overlap
+	double l = r1 - r2 - (c1 - c2).length();
+	return (l > eps ? 1 : (abs(l) < eps ? -1 : 0));
+}
+
+int circleOutsideCircle(const point & c1, double r1, const point & c2, double r2){
+	//test if circle 2 is outside circle 1
+	//returns "-1" if they touch externally, "1" if 2 is outside 1, "0" if they overlap
+	double l = (c1 - c2).length() - (r1 + r2);
+	return (l > eps ? 1 : (abs(l) < eps ? -1 : 0));
+}
+
+vector<vector<point>> commonExteriorTangents(const point & c1, double r1, const point & c2, double r2){
+	//returns a vector of segments or a single point
+	if(r1 < r2) return commonExteriorTangents(c2, r2, c1, r1);
+	if(c1 == c2 && abs(r1-r2) < 0) return {};
+	int in = circleInsideCircle(c1, r1, c2, r2);
+	if(in == 1) return {};
+	else if(in == -1) return {{c1 + (c2 - c1).normalize() * r1}};
+	else{
+		pair<point, point> t;
+		if(abs(r1-r2) < eps)
+			t = {c1 - (c2 - c1).perpendicular(), c1 + (c2 - c1).perpendicular()};
+		else
+			t = pointsOfTangency(c2, c1, r1 - r2);
+		t.first = (t.first - c1).normalize();
+		t.second = (t.second - c1).normalize();
+		return {{c1 + t.first * r1, c2 + t.first * r2}, {c1 + t.second * r1, c2 + t.second * r2}};
+	}
+}
+
+vector<vector<point>> commonInteriorTangents(const point & c1, double r1, const point & c2, double r2){
+	if(c1 == c2 && abs(r1-r2) < 0) return {};
+	int out = circleOutsideCircle(c1, r1, c2, r2);
+	if(out == 0) return {};
+	else if(out == -1) return {{c1 + (c2 - c1).normalize() * r1}};
+	else{
+		auto t = pointsOfTangency(c2, c1, r1 + r2);
+		t.first = (t.first - c1).normalize();
+		t.second = (t.second - c1).normalize();
+		return {{c1 + t.first * r1, c2 - t.first * r2}, {c1 + t.second * r1, c2 - t.second * r2}};
+	}
+}
+
+vector<point> minkowskiSum(vector<point> A, vector<point> B){
+	int na = (int)A.size(), nb = (int)B.size();
+	if(A.empty() || B.empty()) return {};
+
+	rotate(A.begin(), min_element(A.begin(), A.end()), A.end());
+	rotate(B.begin(), min_element(B.begin(), B.end()), B.end());
+
+	int pa = 0, pb = 0;
+	vector<point> M;
+
+	while(pa < na && pb < nb){
+		M.push_back(A[pa] + B[pb]);
+		double x = (A[(pa + 1) % na] - A[pa]).cross(B[(pb + 1) % nb] - B[pb]);
+		if(x <= eps) pb++;
+		if(-eps <= x) pa++;
+	}
+
+	while(pa < na) M.push_back(A[pa++] + B[0]);
+	while(pb < nb) M.push_back(B[pb++] + A[0]);
+
+	return M;
+}
+
+int main(){
+	vector<pair<point, point>> centers = {{point(-2, 5), point(-8, -7)}, {point(14, 4), point(18, 6)}, {point(9, 20), point(9, 28)},
+										  {point(21, 20), point(21, 29)}, {point(8, -10), point(14, -10)}, {point(24, -6), point(34, -6)},
+										  {point(34, 8), point(36, 9)}, {point(50, 20), point(56, 24.5)}};
+	vector<pair<double, double>> radii = {{7, 4}, {3, 5}, {4, 4}, {4, 5}, {3, 3}, {4, 6}, {5, 1}, {10, 2.5}};
+	int n = centers.size();
+	for(int i = 0; i < n; ++i){
+		cout << "\n" << centers[i].first << " " << radii[i].first << " " << centers[i].second << " " << radii[i].second << "\n";
+		auto extLines = commonExteriorTangents(centers[i].first, radii[i].first, centers[i].second, radii[i].second);
+		cout << "Exterior tangents:\n";
+		for(auto par : extLines){
+			for(auto p : par){
+				cout << p << " ";
+			}
+			cout << "\n";
+		}
+		auto intLines = commonInteriorTangents(centers[i].first, radii[i].first, centers[i].second, radii[i].second);
+		cout << "Interior tangents:\n";
+		for(auto par : intLines){
+			for(auto p : par){
+				cout << p << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
 }
---
+++ simplex.cpp
@@ -0,0 +1,123 @@
+/*
+Parametric Self-Dual Simplex method
+Solve a canonical LP:
+	min or max. c x
+	s.t. A x <= b
+		x >= 0
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const double eps = 1e-9, oo = numeric_limits<double>::infinity();
+
+typedef vector<double> vec;
+typedef vector<vec> mat;
+
+pair<vec, double> simplexMethodPD(mat &A, vec &b, vec &c, bool mini = true){
+	int n = c.size(), m = b.size();
+	mat T(m + 1, vec(n + m + 1));
+	vector<int> base(n + m), row(m);
+
+	for(int j = 0; j < m; ++j){
+		for(int i = 0; i < n; ++i)
+			T[j][i] = A[j][i];
+		row[j] = n + j;
+		T[j][n + j] = 1;
+		base[n + j] = 1;
+		T[j][n + m] = b[j];
+	}
+
+	for(int i = 0; i < n; ++i)
+		T[m][i] = c[i] * (mini ? 1 : -1);
+
+	while(true){
+		int p = 0, q = 0;
+		for(int i = 0; i < n + m; ++i)
+			if(T[m][i] <= T[m][p])
+				p = i;
+
+		for(int j = 0; j < m; ++j)
+			if(T[j][n + m] <= T[q][n + m])
+				q = j;
+
+		double t = min(T[m][p], T[q][n + m]);
+
+		if(t >= -eps){
+			vec x(n);
+			for(int i = 0; i < m; ++i)
+				if(row[i] < n) x[row[i]] = T[i][n + m];
+			return {x, T[m][n + m] * (mini ? -1 : 1)}; // optimal
+		}
+
+		if(t < T[q][n + m]){
+			// tight on c -> primal update
+			for(int j = 0; j < m; ++j)
+				if(T[j][p] >= eps)
+					if(T[j][p] * (T[q][n + m] - t) >= T[q][p] * (T[j][n + m] - t))
+						q = j;
+
+			if(T[q][p] <= eps)
+				return {vec(n), oo * (mini ? 1 : -1)}; // primal infeasible
+		}else{
+			// tight on b -> dual update
+			for(int i = 0; i < n + m + 1; ++i)
+				T[q][i] = -T[q][i];
+
+			for(int i = 0; i < n + m; ++i)
+				if(T[q][i] >= eps)
+					if(T[q][i] * (T[m][p] - t) >= T[q][p] * (T[m][i] - t))
+						p = i;
+
+			if(T[q][p] <= eps)
+				return {vec(n), oo * (mini ? -1 : 1)}; // dual infeasible
+		}
+
+		for(int i = 0; i < m + n + 1; ++i)
+			if(i != p) T[q][i] /= T[q][p];
+
+		T[q][p] = 1; // pivot(q, p)
+		base[p] = 1;
+		base[row[q]] = 0;
+		row[q] = p;
+
+		for(int j = 0; j < m + 1; ++j){
+			if(j != q){
+				double alpha = T[j][p];
+				for(int i = 0; i < n + m + 1; ++i)
+					T[j][i] -= T[q][i] * alpha;
+			}
+		}
+	}
+
+	return {vec(n), oo};
+}
+
+int main(){
+	int m, n;
+	bool mini = true;
+	cout << "Numero de restricciones: ";
+	cin >> m;
+	cout << "Numero de incognitas: ";
+	cin >> n;
+	mat A(m, vec(n));
+	vec b(m), c(n);
+	for(int i = 0; i < m; ++i){
+		cout << "Restriccion #" << (i + 1) << ": ";
+		for(int j = 0; j < n; ++j){
+			cin >> A[i][j];
+		}
+		cin >> b[i];
+	}
+	cout << "[0]Max o [1]Min?: ";
+	cin >> mini;
+	cout << "Coeficientes de " << (mini ? "min" : "max") << " z: ";
+	for(int i = 0; i < n; ++i){
+		cin >> c[i];
+	}
+	cout.precision(6);
+	auto ans = simplexMethodPD(A, b, c, mini);
+	cout << (mini ? "Min" : "Max") << " z = " << ans.second << ", cuando: \n";
+	for(int i = 0; i < ans.first.size(); ++i){
+		cout << "x_" << (i + 1) << " = " << ans.first[i] << "\n";
+	}
+	return 0;
+}
