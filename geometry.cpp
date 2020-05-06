77b5ebeaebf10d9329d5d69623eee54f2caed87b
Alan Enrique Ontiveros Salazar <alan-enrique@live.com.mx>
2020-05-06 02:43:28 -0500

    Update

--- geometry.cpp
+++ geometry.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ld = long double;
-ld eps = 1e-9, inf = numeric_limits<ld>::max();
+const ld eps = 1e-9, inf = numeric_limits<ld>::max(), pi = acos(-1);
 // For use with integers, just set eps=0 and everything remains the same
 bool geq(ld a, ld b){return a-b >= -eps;}     //a >= b
 bool leq(ld a, ld b){return b-a >= -eps;}     //a <= b
@@ -25,11 +25,11 @@ struct point{
 	point operator*=(const ld & p){*this = *this * p; return *this;}
 	point operator/=(const ld & p){*this = *this / p; return *this;}
 
-	point rotate(const ld & angle) const{
-		return point(x * cos(angle) - y * sin(angle), x * sin(angle) + y * cos(angle));
-	}
+	point rotate(const ld & a) const{return point(x*cos(a) - y*sin(a), x*sin(a) + y*cos(a));}
 	point perp() const{return point(-y, x);}
-
+	ld ang() const{
+		ld a = atan2l(y, x); a += le(a, 0) ? 2*pi : 0; return a;
+	}
 	ld dot(const point & p) const{return x * p.x + y * p.y;}
 	ld cross(const point & p) const{return x * p.y - y * p.x;}
 	ld norm() const{return x * x + y * y;}
@@ -769,6 +769,80 @@ vector<tuple<point, point, point>> delaunay(vector<point> & P){
 	}
 	return ans;
 }
+
+struct circ{
+	point c;
+	ld r;
+	circ() {}
+	circ(const point & c, ld r): c(c), r(r) {}
+	set<pair<ld, ld>> ranges;
+
+	void disable(ld l, ld r){
+		ranges.emplace(l, r);
+	}
+
+	auto getActive() const{
+		vector<pair<ld, ld>> ans;
+		ld maxi = 0;
+		for(const auto & dis : ranges){
+			ld l, r;
+			tie(l, r) = dis;
+			if(l > maxi){
+				ans.emplace_back(maxi, l);
+			}
+			maxi = max(maxi, r);
+		}
+		if(!eq(maxi, 2*pi)){
+			ans.emplace_back(maxi, 2*pi);
+		}
+		return ans;
+	}
+};
+
+ld areaUnionCircles(const vector<circ> & circs){
+	vector<circ> valid;
+	for(const circ & curr : circs){
+		if(eq(curr.r, 0)) continue;
+		circ nuevo = curr;
+		for(circ & prev : valid){
+			if(circleInsideCircle(prev.c, prev.r, nuevo.c, nuevo.r)){
+				nuevo.disable(0, 2*pi);
+			}else if(circleInsideCircle(nuevo.c, nuevo.r, prev.c, prev.r)){
+				prev.disable(0, 2*pi);
+			}else{
+				auto cruce = intersectionCircles(prev.c, prev.r, nuevo.c, nuevo.r);
+				if(cruce.size() == 2){
+					ld a1 = (cruce[0] - prev.c).ang();
+					ld a2 = (cruce[1] - prev.c).ang();
+					ld b1 = (cruce[1] - nuevo.c).ang();
+					ld b2 = (cruce[0] - nuevo.c).ang();
+					if(a1 < a2){
+						prev.disable(a1, a2);
+					}else{
+						prev.disable(a1, 2*pi);
+						prev.disable(0, a2);
+					}
+					if(b1 < b2){
+						nuevo.disable(b1, b2);
+					}else{
+						nuevo.disable(b1, 2*pi);
+						nuevo.disable(0, b2);
+					}
+				}
+			}
+		}
+		valid.push_back(nuevo);
+	}
+	ld ans = 0;
+	for(const circ & curr : valid){
+		for(const auto & range : curr.getActive()){
+			ld l, r;
+			tie(l, r) = range;
+			ans += curr.r*(curr.c.x * (sin(r) - sin(l)) - curr.c.y * (cos(r) - cos(l))) + curr.r*curr.r*(r-l);
+		}
+	}
+	return ans/2;
+};
 
 int main(){
 	/*vector<pair<point, point>> centers = {{point(-2, 5), point(-8, -7)}, {point(14, 4), point(18, 6)}, {point(9, 20), point(9, 28)},
